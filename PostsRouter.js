e6a7537d8fe56321004a1db6b494860185911895
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-10-01 14:34:58 -0700

    figured out my issue

--- PostsRouter.js
+++ PostsRouter.js
@@ -15,35 +15,29 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    db.insert(req.body)
+    const info = req.body;
+    console.log('info', info)
+    if(!info.title || !info.contents){
+        res.status(400).json({message: "Please provide title and contents for the post"})
+    }else{
+        db.insert(req.body)
     .then(data => {
-        if(!title || !contents){
-            res.status(400).json({message: "Please provide title and contents for the post"})
-        }else{
-            res.status(201).json(data)
-        }
+        res.status(201).json(info)
     })
-    .catch(error => {
-        res.status(500).json({error: "There was an error while saving the post to the database"})
-    });
-});
+}}),
+    
+    
 
-// router.post('/api/posts/:id/comments', (req, res) => {
-//     const id = req.params.id;
-//     data.insert(id)
-//     .then(data => {
-//         if(data){
-//             res.status(201).json(data)
-//         }if(!text){
-//             res.status(400).json({errorMessage: "Please provide text for the comment"})
-//         }else{
-//             res.status(404).json({message: "The post with the specified ID does not exist."})
-//         }
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json({message: 'Error adding the data'})
-//     })
-// })
+router.post('/api/posts/:id/comments', (req, res) => {
+    const id = req.params.id;
+    data.insert(id)
+    .then(data => {
+        
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({message: 'Error adding the data'})
+    })
+})
 
 module.exports = router;
